e983c0b92918212c21c5015a86830f203a4dc095
Bryan Crampton <bryancrampton@google.com>
2017-10-13 16:21:21 -0700

    Add labels object to QueryJobsRequest

--- ui/src/app/shared/model/QueryJobsRequest.ts
+++ ui/src/app/shared/model/QueryJobsRequest.ts
@@ -32,6 +32,11 @@ export interface QueryJobsRequest {
     statuses?: Array<models.JobStatus>;
 
     /**
+     * If specified, returns only jobs matching all of these labels. 
+     */
+    labels?: any;
+
+    /**
      * Returns only jobs with the specified name. 
      */
     name?: string;
