61304e1c62489916ff0b31214a1573f63396f41f
Alan Hwang <alanhwang@google.com>
2017-10-16 13:37:33 -0700

    Fix routing bug

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import {JobListComponent} from './job-list/job-list.component';
 // Based on the URL mapping in "routes" below, the RouterModule attaches
 // UI Components to the <router-outlet> element in the main AppComponent.
 const routes: Routes = [
-  {path: '', redirectTo: '/jobs', pathMatch: 'full'},
+  {path: '', redirectTo: 'jobs', pathMatch: 'full'},
   {path: 'jobs', component:JobListComponent},
   {path: 'jobs/:id', component: JobDetailsComponent, resolve: {
     job: JobDetailsResolver
