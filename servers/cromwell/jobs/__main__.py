f9c4608ab49ed54585ec30628070ea0ef14add6d
Saman Ehsan <se8ea@virginia.edu>
2017-10-26 17:41:12 -0400

    Modify files auto-generated by swagger-codegen

--- servers/cromwell/jobs/__main__.py
+++ servers/cromwell/jobs/__main__.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
+import os
+import json
 import connexion
 from .encoder import JSONEncoder
 
 
 if __name__ == '__main__':
     app = connexion.App(__name__, specification_dir='./swagger/')
+    config_path = os.environ.get('CROMWELL_CREDENTIALS')
+    with open(config_path) as f:
+        config = json.load(f)
+        app.app.config.update(config)
+    app.app.config['cromwell_url'] = 'https://cromwell.mint-dev.broadinstitute.org/api/workflows/v1'
     app.app.json_encoder = JSONEncoder
-    app.add_api('swagger.yaml', arguments={'title': 'Job Monitor API for interacting with asynchronous batch jobs and workflows.'})
-    app.run(port=8080)
+    app.add_api('swagger.yaml', base_path='/api/v1')
+    app.run(host='0.0.0.0', port=8190)
