215bcd137b263a477adacec095a921165f1ca7a7
Saman Ehsan <se8ea@virginia.edu>
2017-10-30 16:10:21 -0400

    Implement cromwell query endpoint

--- servers/cromwell/jobs/controllers/jobs_controller.py
+++ servers/cromwell/jobs/controllers/jobs_controller.py
@@ -1,46 +1,84 @@
+import requests
 import connexion
-from jobs.models.job_metadata_response import JobMetadataResponse
+from requests.auth import HTTPBasicAuth
+from flask import current_app
+from datetime import datetime
+
+from jobs.models.query_jobs_result import QueryJobsResult
 from jobs.models.query_jobs_request import QueryJobsRequest
 from jobs.models.query_jobs_response import QueryJobsResponse
-from datetime import date, datetime
-from typing import List, Dict
-from six import iteritems
-from ..util import deserialize_date, deserialize_datetime
 
 
 def abort_job(id):
     """
     Abort a job by ID
-    
+
     :param id: Job ID
     :type id: str
 
     :rtype: None
     """
-    return 'do some magic!'
+    return 'abort job'
 
 
 def get_job(id):
     """
     Query for job and task-level metadata for a specified job
-    
+
     :param id: Job ID
     :type id: str
 
     :rtype: JobMetadataResponse
     """
-    return 'do some magic!'
+    return 'get job'
 
 
 def query_jobs(body):
     """
-    Query jobs by various filter criteria. Returned jobs are ordered from newest to oldest submission time. 
-    
-    :param body: 
+    Query jobs by various filter criteria. Returned jobs are ordered from newest to oldest submission time.
+
+    :param body:
     :type body: dict | bytes
 
     :rtype: QueryJobsResponse
     """
-    if connexion.request.is_json:
-        body = QueryJobsRequest.from_dict(connexion.request.get_json())
-    return 'do some magic!'
+    config = current_app.config
+    query = QueryJobsRequest.from_dict(body)
+    query_url = config['cromwell_url'] + '/query'
+    query_params = []
+    if query.start:
+        query_params.append({'start': query.start})
+    if query.end:
+        query_params.append({'end': query.end})
+    if query.name:
+        query_params.append({'name': query.name})
+    if query.statuses:
+        statuses = [{'status': s} for s in set(query.statuses)]
+        query_params.extend(statuses)
+    response = requests.post(query_url, json=query_params, auth=HTTPBasicAuth(config['cromwell_user'], config['cromwell_password']))
+    results = [format_job(job) for job in response.json()['results']]
+    return QueryJobsResponse(results=results)
+
+
+def format_job(job):
+    start = parse_datetime(job.get('start'))
+    end = None
+    if job.get('end'):
+        end = parse_datetime(job.get('end'))
+    return QueryJobsResult(
+        id=job.get('id'),
+        name=job.get('name'),
+        status=job.get('status'),
+        submission=start,
+        start=start,
+        end=end
+    )
+
+
+def parse_datetime(date_string):
+    # Handles issue where some dates in cromwell do not contain milliseconds
+    try:
+        formatted_date = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S.%fZ')
+    except ValueError:
+        formatted_date = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+    return formatted_date
