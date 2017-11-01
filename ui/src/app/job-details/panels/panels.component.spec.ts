b743d1b2ccc9e1cacf08fc72db1efbde695ff861
Alan Hwang <alanhwang@google.com>
2017-11-01 11:07:20 -0700

    Unit test for panels component (#97)
    * Unit test for panels component
    
    * Follow up changes 1

---
+++ ui/src/app/job-details/panels/panels.component.spec.ts
@@ -0,0 +1,138 @@
+import {TestBed, async, ComponentFixture} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+import {Component, DebugElement, ViewChild} from '@angular/core';
+import {JobPanelsComponent} from './panels.component';
+import {MdButtonModule, MdCardModule, MdMenuModule} from '@angular/material';
+
+import {SharedModule} from '../../shared/shared.module';
+import {JobStatus} from '../../shared/model/JobStatus';
+import {JobMetadataResponse} from '../../shared/model/JobMetadataResponse';
+
+describe('JobPanelsComponent', () => {
+
+  let testComponent: TestPanelsComponent;
+  let fixture: ComponentFixture<TestPanelsComponent>;
+  let minimalJob: JobMetadataResponse =
+    {
+      id: 'JOB1',
+      status: JobStatus.Running,
+      submission: new Date('1994-03-29T20:30:00'),
+      start: new Date('1994-03-29T21:00:00')
+    };
+  let completeJob: JobMetadataResponse =
+    {
+      id: 'JOB1',
+      status: JobStatus.Aborted,
+      submission: new Date('1994-03-29T20:30:00'),
+      name: 'Job 1 name',
+      start: new Date('1994-03-29T21:00:00'),
+      end: new Date('1994-03-29T22:00:00'),
+      inputs: {'input': 'gs://input/url/'},
+      outputs: {'output': 'gs://output/url/'},
+      labels: {'user-id': 'user-1'},
+    };
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        JobPanelsComponent,
+        TestPanelsComponent
+      ],
+      imports: [
+        MdButtonModule,
+        MdCardModule,
+        MdMenuModule,
+        SharedModule,
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestPanelsComponent);
+    testComponent = fixture.componentInstance;
+  });
+
+  it('should display with minimal job', async(() => {
+    testComponent.job = minimalJob;
+    fixture.detectChanges();
+    let de: DebugElement = fixture.debugElement;
+    expect(de.query(By.css('.header')).nativeElement.textContent).toEqual('');
+    expect(de.query(By.css('.job-id')).nativeElement.textContent)
+      .toContain(minimalJob.id);
+    expect(de.query(By.css('.text-info')).nativeElement.textContent)
+      .toEqual('Submitted by: ');
+  }));
+
+  it('should hide input buttons with minimal job', async(() => {
+    testComponent.job = minimalJob;
+    fixture.detectChanges();
+    expect(testComponent.jobPanelsComponent.showInputsButton()).toBeFalsy();
+    expect(testComponent.jobPanelsComponent.showOutputsButton()).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('.view-resources-button')))
+      .toBeNull();
+  }));
+
+  it('should display all features with complete job', async(() => {
+    testComponent.job = completeJob;
+    fixture.detectChanges();
+    let de: DebugElement = fixture.debugElement;
+    expect(de.query(By.css('.text-info')).nativeElement.textContent)
+      .toContain(completeJob.labels['user-id']);
+    expect(de.query(By.css('#ended')).nativeElement.textContent)
+      .toContain("10:00 PM");
+  }));
+
+  it('should show input buttons with complete job', async(() => {
+    testComponent.job = completeJob;
+    fixture.detectChanges();
+    expect(testComponent.jobPanelsComponent.showInputsButton()).toBeTruthy();
+    expect(testComponent.jobPanelsComponent.showOutputsButton()).toBeTruthy();
+    expect(fixture.debugElement.queryAll(By.css('.view-resources-button')).length)
+      .toEqual(2);
+  }));
+
+  it('should render correct GCS resource URLs', async(() => {
+    testComponent.job = completeJob;
+    fixture.detectChanges();
+    expect(testComponent.jobPanelsComponent.getInputResourceURL('input'))
+      .toEqual('https://console.cloud.google.com/storage/browser/input/url');
+    expect(testComponent.jobPanelsComponent.getOutputResourceURL('output'))
+      .toEqual('https://console.cloud.google.com/storage/browser/output/url');
+  }));
+
+  it('should return invalid from invalid resource url', async(() => {
+    expect(testComponent.jobPanelsComponent.getResourceURL('/invalid/url'))
+      .toBeUndefined();
+  }));
+
+  it('should return correct value from getUserId', async(() => {
+    testComponent.job = completeJob;
+    fixture.detectChanges();
+    expect(testComponent.jobPanelsComponent.getUserId(testComponent.job))
+      .toEqual(completeJob.labels['user-id']);
+  }));
+
+  it('should calculate job duration', async(() => {
+    testComponent.job = completeJob;
+    fixture.detectChanges();
+    expect(testComponent.jobPanelsComponent.getDuration()).toEqual("2h 30m");
+  }));
+
+
+
+  @Component({
+    selector: 'jm-test-panels-component',
+    template: `<jm-panels [job]="job"></jm-panels>`
+  })
+  class TestPanelsComponent {
+    public job: JobMetadataResponse = {
+      id: '',
+      status: JobStatus.Failed,
+      submission: new Date(),
+      start: new Date()
+    };
+    @ViewChild(JobPanelsComponent)
+    public jobPanelsComponent: JobPanelsComponent;
+  }
+
+});
--- ui/src/app/job-details/panels/panels.component.ts
+++ ui/src/app/job-details/panels/panels.component.ts
@@ -34,9 +34,8 @@ export class JobPanelsComponent implements OnChanges {
         }
       }
     }
-
-    this.inputs = Object.keys(this.job.inputs).sort();
-    this.outputs = Object.keys(this.job.outputs).sort();
+    this.inputs = Object.keys(this.job.inputs || {}).sort();
+    this.outputs = Object.keys(this.job.outputs || {}).sort();
   }
 
   getDuration(): String {
@@ -71,11 +70,15 @@ export class JobPanelsComponent implements OnChanges {
     return this.gcsPrefix + parts.slice(2,-1).join("/");
   }
 
+  getUserId(job: JobMetadataResponse) {
+    return job.labels ? job.labels['user-id'] : "";
+  }
+
   showInputsButton(): boolean {
-    return this.inputs.length > 0;
+    return this.inputs && this.inputs.length > 0;
   }
 
   showOutputsButton(): boolean {
-    return this.outputs.length > 0;
+    return this.outputs && this.outputs.length > 0;
   }
 }
