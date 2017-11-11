632b37904b1446fccfb74a7bcbe549d0c0d5aac4
CH Albach <calbach@google.com>
2017-11-10 16:37:04 -0800

    Refactor docker to dev/prod; support prod images

---
+++ ui/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+// Placeholder file. Specific production deployments should overwrite this file
+// when building the UI server as it contains deployment-specific details.
+
+export const environment = {}
