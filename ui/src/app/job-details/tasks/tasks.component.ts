cc64a6d3deb009a772eca4722f69f2fed8ceb147
Saman Ehsan <se8ea@virginia.edu>
2017-11-10 19:02:20 -0500

    Update information displayed for tasks

--- ui/src/app/job-details/tasks/tasks.component.ts
+++ ui/src/app/job-details/tasks/tasks.component.ts
@@ -20,14 +20,17 @@ import {TaskMetadata} from '../../shared/model/TaskMetadata';
 })
 export class TaskDetailsComponent implements OnInit, OnChanges {
   @Input() tasks: TaskMetadata[] = [];
+  browserPrefix: string = "https://console.cloud.google.com/storage/browser/";
   database = new TasksDatabase(this.tasks);
   dataSource: TasksDataSource | null;
   displayedColumns = [
     'name',
     'status',
-    'shard',
-    'label1',
-    'label2',
+    'startTime',
+    'duration',
+    'attempts',
+    'stdout',
+    'stderr'
   ];
 
   ngOnInit() {
@@ -42,6 +45,48 @@ export class TaskDetailsComponent implements OnInit, OnChanges {
   getStatusUrl(status: JobStatus): string {
     return JobStatusImage[status];
   }
+
+  getDuration(task: TaskMetadata): String {
+    let duration: number;
+    if (task.end) {
+      duration = task.end.getTime() - task.start.getTime();
+    } else {
+      duration = new Date().getTime() - task.start.getTime();
+    }
+    return Math.round(duration/3600000) + "h " +
+      Math.round(duration/60000)%60 + "m";
+  }
+
+  getResourceBrowserURL(uri: string): string {
+    let parts = this.validateGcsURLGetParts(uri);
+    // This excludes the object from the link to show the enclosing directory.
+    // This is valid with wildcard glob (bucket/path/*) and directories
+    // (bucket/path/dir/) as well, the * or empty string will be trimmed.
+    return parts ? this.browserPrefix + parts.slice(2,-1).join("/") : undefined;
+  }
+
+  private validateGcsURLGetParts(url: string): string[] {
+    if (typeof(url) !== 'string') {
+      return;
+    }
+    let parts = url.split("/");
+    if (parts[0] != "gs:" || parts[1] != "") {
+      // TODO(bryancrampton): Handle invalid resource URL gracefully
+      return;
+    }
+    return parts;
+  }
+
+  formatValue(value: string): string {
+    let parts = this.validateGcsURLGetParts(value);
+    let formattedValue = value;
+    if (parts) {
+      // display the file name instead of the full resourceURL
+      formattedValue = parts[parts.length -1];
+    }
+    return formattedValue;
+  }
+
 }
 
 /** Simple database with an observable list of jobs to be subscribed to by the
