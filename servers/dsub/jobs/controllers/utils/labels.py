898cd08cbc7213aea3830a682271886c69d62904
Bryan Crampton <bryancrampton@google.com>
2018-02-16 18:32:07 -0500

    [dsub] Implement changes to returned job models for capabilities update (#211)

--- servers/dsub/jobs/controllers/utils/labels.py
+++ servers/dsub/jobs/controllers/utils/labels.py
@@ -7,18 +7,9 @@ def dsub_to_api(job):
         Returns:
             dict: Labels key value pairs with dsub-specific information
     """
-    # Put any dsub specific information into the labels. These fields are
-    # candidates for the common jobs API
     labels = job['labels'].copy() if job['labels'] else {}
-    if 'status-detail' in job:
-        labels['status-detail'] = job['status-detail']
-    if 'last-update' in job:
-        labels['last-update'] = job['last-update']
-    if 'user-id' in job:
-        labels['user-id'] = job['user-id']
     if 'job-id' in job:
         labels['job-id'] = job['job-id']
     if 'task-id' in job:
         labels['task-id'] = job['task-id']
-
     return labels
