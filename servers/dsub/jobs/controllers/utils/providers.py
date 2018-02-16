df0b84689d2740f556957933a38f8bc0f23c6dec
Bryan Crampton <bryancrampton@google.com>
2018-02-16 18:32:07 -0500

    [dsub] Implement changes to returned job models for capabilities update (#211)

--- servers/dsub/jobs/controllers/utils/providers.py
+++ servers/dsub/jobs/controllers/utils/providers.py
@@ -10,12 +10,12 @@ from jobs.common import enum
 ProviderType = enum(GOOGLE='google', LOCAL='local', STUB='stub')
 
 
-def get_provider(provider_type, parent_id=None, auth_token=None):
+def get_provider(provider_type, project_id=None, auth_token=None):
     """Construct the dsub provider for the given parameters.
 
         Args:
             provider_type: A string indicating google, local, or stub provider
-            parent_id: A string representing a Google Cloud Project ID
+            project_id: A string representing a Google Cloud Project ID
             auth_token: oauth2 token for authorizing Genomics API requests in
                 dsub
 
@@ -24,8 +24,8 @@ def get_provider(provider_type, parent_id=None, auth_token=None):
                 StubJobProvider.
     """
     if provider_type == ProviderType.GOOGLE:
-        return _get_google_provider(parent_id, auth_token)
-    elif parent_id or auth_token:
+        return _get_google_provider(project_id, auth_token)
+    elif project_id or auth_token:
         raise BadRequest(
             'The Local provider does not support the `{}` field .'.format(
                 'authToken' if auth_token else 'parentId'))
@@ -37,13 +37,13 @@ def get_provider(provider_type, parent_id=None, auth_token=None):
         return stub.StubJobProvider()
 
 
-def _get_google_provider(parent_id, auth_token):
-    if not parent_id:
-        raise BadRequest('Missing required field `parentId`.')
+def _get_google_provider(project_id, auth_token):
+    if not project_id:
+        raise BadRequest('Missing required field `extensions.projectId`.')
     if not auth_token:
         if _requires_auth():
             raise BadRequest('Missing required field `authToken`.')
-        return google.GoogleJobProvider(False, False, parent_id)
+        return google.GoogleJobProvider(False, False, project_id)
 
     resp = requests.post(
         'https://www.googleapis.com/oauth2/v2/tokeninfo',
@@ -57,7 +57,7 @@ def _get_google_provider(parent_id, auth_token):
     try:
         credentials = AccessTokenCredentials(auth_token, 'user-agent')
         return google.GoogleJobProvider(
-            False, False, parent_id, credentials=credentials)
+            False, False, project_id, credentials=credentials)
     except AccessTokenCredentialsError as e:
         raise Unauthorized('Invalid authentication token:{}.'.format(e))
 
