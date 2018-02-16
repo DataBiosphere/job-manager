8ca005b84d36c4379724b0fc3cba550fa326acac
Bryan Crampton <bryancrampton@google.com>
2018-02-16 10:00:04 -0800

    [cromwell] Implement capabilities endpoint

--- servers/cromwell/jobs/controllers/capabilities_controller.py
+++ servers/cromwell/jobs/controllers/capabilities_controller.py
@@ -1,7 +1,20 @@
+from jobs.models.capabilities_response import CapabilitiesResponse
+
+
 def get_capabilities():
     """Get the capabilities of this backend implementation.
 
     Returns:
         CapabilitiesResponse: Response containing this backend's capabilities
     """
-    raise NotImplemented('Not yet supported.')
+    return CapabilitiesResponse(
+        display_fields={
+            'name': 'Job',
+            'status': 'Status',
+            'submission': 'Submitted',
+            'labels.cromwell-workflow-name': "Workflow Name",
+            'labels.cromwell-workflow-id': "Workflow ID",
+            'labels.comment': "Comment"
+        },
+        common_labels=[],
+        extended_query_fields=[])
