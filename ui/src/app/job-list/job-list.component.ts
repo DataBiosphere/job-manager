2f7557ddbeb1b9b48f96adf5b3a004e220c8f74e
CH Albach <calbach@google.com>
2018-02-20 12:41:33 -0800

    Fix ng build visibility issues

--- ui/src/app/job-list/job-list.component.ts
+++ ui/src/app/job-list/job-list.component.ts
@@ -29,7 +29,7 @@ export class JobListComponent implements OnInit {
 
   // This Subject is synchronized to a JobStream, which we destroy and recreate
   // whenever we change query parameters, via a subscription.
-  private jobs = new BehaviorSubject<JobListView>({
+  jobs = new BehaviorSubject<JobListView>({
     results: [],
     exhaustive: false
   });
