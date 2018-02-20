177ee4953f466b2650a37d9c24cba08877ec52ed
Bryan Crampton <bryancrampton@google.com>
2018-02-20 10:57:57 -0800

    Make labels queryable

--- servers/cromwell/jobs/controllers/capabilities_controller.py
+++ servers/cromwell/jobs/controllers/capabilities_controller.py
@@ -8,13 +8,18 @@ def get_capabilities():
         CapabilitiesResponse: Response containing this backend's capabilities
     """
     return CapabilitiesResponse(
-        display_fields={
-            'name': 'Job',
-            'status': 'Status',
-            'submission': 'Submitted',
-            'labels.cromwell-workflow-name': "Workflow Name",
-            'labels.cromwell-workflow-id': "Workflow ID",
-            'labels.comment': "Comment"
-        },
-        common_labels=[],
+        display_fields=[
+            DisplayField(field='name', display='Job'),
+            DisplayField(field='status', display='Status'),
+            DisplayField(field='submission', display='Submitted'),
+            DisplayField(
+                field='labels.cromwell-workflow-name',
+                display='Workflow Name'),
+            DisplayField(
+                field='labels.cromwell-workflow-id', display='Workflow ID'),
+            DisplayField(field='labels.comment', display='Comment')
+        ],
+        common_labels=[
+            'cromwell-workflow-name', 'cromwell-workflow-id', 'comment'
+        ],
         extended_query_fields=[])
