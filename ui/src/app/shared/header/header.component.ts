0155bd3feb5a81d4a83e884b311e144c7f3b4a0e
CH Albach <calbach@google.com>
2018-02-22 14:54:52 -0800

    Fix initial non-exhaustive paginator rendering

--- ui/src/app/shared/header/header.component.ts
+++ ui/src/app/shared/header/header.component.ts
@@ -276,11 +276,9 @@ export class JobsPaginatorIntl extends MatPaginatorIntl {
               public changes: Subject<void>) {
     super();
     backendJobs.subscribe((jobList: JobListView) => {
-      // Ensure that the paginator component is redrawn once we transition to
-      // an exhaustive list of jobs.
-      if (jobList.exhaustive) {
-        changes.next();
-      }
+      // Ensure that the paginator component is redrawn on initialization and
+      // when the data changes, e.g. to catch the transition to exhaustive.
+      changes.next();
     });
   }
 
