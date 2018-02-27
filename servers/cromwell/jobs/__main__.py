d285276dc16aa80b508469f43a5d8dc379fc7857
Rex Wang <rexwangcc@gmail.com>
2018-02-27 17:51:28 -0500

    Fix issues for Cromwell API deployments (#224)
    * Add startup checks by moving the entrypoint of gunicorn to __init__ to prevent Cromwell shim layer from blowing up.
    * Remove debug line.
    * Add more instructions to readme.

--- servers/cromwell/jobs/__main__.py
+++ servers/cromwell/jobs/__main__.py
@@ -5,6 +5,12 @@ import os
 import json
 import connexion
 from .encoder import JSONEncoder
+import requests
+from requests.auth import HTTPBasicAuth
+import logging
+
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger("{module_path}".format(module_path=__name__))
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -31,13 +37,44 @@ else:
     args, _ = parser.parse_known_args()
 
 app = connexion.App(__name__, specification_dir='./swagger/', swagger_ui=False)
+DEFAULT_CROMWELL_CREDENTIALS = {'cromwell_user': '', 'cromwell_password': ''}
+
+# Load credentials for cromwell
 config_path = os.environ.get('CROMWELL_CREDENTIALS')
-with open(config_path) as f:
-    config = json.load(f)
+
+# Check if the credentials are provided properly
+try:
+    with open(config_path) as f:
+        config = json.load(f)
+except (IOError, TypeError):
+    logger.warning(
+        'Failed to load config.json, using the default config: {}'.format(
+            DEFAULT_CROMWELL_CREDENTIALS))
+    config = DEFAULT_CROMWELL_CREDENTIALS
+finally:
     app.app.config.update(config)
+
 app.app.config['cromwell_url'] = args.cromwell_url
 app.app.json_encoder = JSONEncoder
 app.add_api('swagger.yaml', base_path=args.path_prefix)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=args.port)
+
+def run():
+    # Check the connections with cromwell
+    try:
+        response = requests.head(
+            args.cromwell_url,
+            auth=HTTPBasicAuth(app.app.config['cromwell_user'],
+                               app.app.config['cromwell_password']),
+            timeout=5)
+        if response.status_code == 401:
+            raise requests.exceptions.HTTPError(
+                'Invalid credentials for the Cromwell: {}'.format(
+                    args.cromwell_url))
+        return app.app
+    except KeyError:
+        logger.error('Invalid config.json file provided.')
+    except requests.exceptions.RequestException as err:
+        logger.critical(err)
+        logger.critical('Failed to connect to Cromwell: {}'.format(
+            args.cromwell_url))
