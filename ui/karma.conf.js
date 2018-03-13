56897ee704408912af7c8f0299b54dee6de1b1e0
CH Albach <calbach@users.noreply.github.com>
2018-03-13 12:28:16 +0900

    Support ng test with chrome headless, use for Circle (#232)

--- ui/karma.conf.js
+++ ui/karma.conf.js
@@ -3,6 +3,17 @@
 
 module.exports = function (config) {
   config.set({
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          // Without a remote debugging port, Google Chrome exits immediately.
+          '--remote-debugging-port=9222',
+        ],
+      }
+    },
     basePath: '',
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
