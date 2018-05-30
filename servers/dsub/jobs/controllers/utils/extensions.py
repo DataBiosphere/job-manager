2f60f5087218bb99471ec11ce86bce8bd3fe932f
Zachary <hsia.zachary@gmail.com>
2018-05-30 16:58:08 -0700

    delete inline-text class, fake data removed

--- servers/dsub/jobs/controllers/utils/extensions.py
+++ servers/dsub/jobs/controllers/utils/extensions.py
@@ -16,39 +16,13 @@ def get_extensions(job):
     if envs and envs['_SCRIPT']:
         script = envs['_SCRIPT']
         del envs['_SCRIPT']
-
+    
+    # TODO: Plumb dstat events.
+    
     return ExtendedFields(
         user_id=job['user-id'],
         status_detail=job.get('status-detail'),
         logs=logs.dsub_to_api(job),
         last_update=job.get('last-update'),
         envs=job['envs'],
-        source_file=script,
-        events=hack_events())
-
-
-def hack_events():
-    """Place hard coded events information.
-
-    """
-    events = [{
-        "description": "start",
-        "startTime": "2018-05-24T21:42:51.109104814Z"
-    }, {
-        "description": "pulling-image",
-        "startTime": "2018-05-24T21:42:51.109147076Z"
-    }, {
-        "description": "localizing-files",
-        "startTime": "2018-05-24T21:43:05.783703332Z"
-    }, {
-        "description": "running-docker",
-        "startTime": "2018-05-24T21:43:05.783735828Z"
-    }, {
-        "description": "delocalizing-files",
-        "startTime": "2018-05-24T21:43:07.447844496Z"
-    }, {
-        "description": "ok",
-        "startTime": "2018-05-24T21:43:10.428214323Z"
-    }]
-
-    return events
+        source_file=script)
