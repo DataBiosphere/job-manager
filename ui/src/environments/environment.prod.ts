dc345f1c0473d4aea01d06b0c19b10ce457e40b4
Rex Wang <chengche@broadinstitute.org>
2018-06-01 10:16:52 -0400

    Bring back the prod flag for ng build.

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 // Placeholder file. Specific production deployments should overwrite this file
 // when building the UI server as it contains deployment-specific details.
 
-export const environment = {}
+export const environment = {
+  apiUrl: '',
+  clientId: '',
+  production: true,
+};
