c9b583ae50d227fdad2fb00717c2c4b7267af1c1
Bryan Crampton <bryancrampton@google.com>
2018-06-11 12:40:42 -0700

    [dsub] Fix local provider

--- servers/dsub/jobs/controllers/capabilities_controller.py
+++ servers/dsub/jobs/controllers/capabilities_controller.py
@@ -24,7 +24,7 @@ def get_capabilities():
                 field='extensions.statusDetail', display='Status Detail'),
         ],
         common_labels=['job-id', 'task-id'],
-        query_extensions=['projectId', 'userId', 'submission'])
+        query_extensions=['userId', 'submission'])
 
     if _provider_type() == ProviderType.GOOGLE:
         capabilities.authentication = AuthenticationCapability(
@@ -34,6 +34,7 @@ def get_capabilities():
                 'https://www.googleapis.com/auth/cloudplatformprojects.readonly',
                 'https://www.googleapis.com/auth/devstorage.read_only',
             ])
+        capabilities.query_extensions.append('projectId')
 
     return capabilities
 
