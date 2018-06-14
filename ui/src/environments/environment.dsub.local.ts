6e37ab7c4dc69af12e6470be98b1bd580c0b5728
Rex Wang <chengche@broadinstitute.org>
2018-06-14 16:06:26 -0400

    Replace environment TS files with JSON config files and update docker compose files.

--- ui/src/environments/environment.dsub.local.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  apiUrl: '/api/v1',
-  clientId: '',
-  production: false,
-  dashboardEnabled: false,
-};
--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -4,8 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  apiUrl: '/api/v1',
-  clientId: '',
   production: false,
   dashboardEnabled: false,
 };
