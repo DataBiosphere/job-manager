0237fedc8aec056ccf878687c1df4f03f36ceaa3
Zachary Hsia <hsia.zachary@gmail.com>
2018-06-27 16:45:08 -0700

    Added link to status counts on dashboard page. (#302)
    Add routerLink to status counts as well as test cases.

--- ui/src/app/dashboard/total-summary/total-summary.component.ts
+++ ui/src/app/dashboard/total-summary/total-summary.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input} from '@angular/core';
 import {StatusCounts} from "../../shared/model/StatusCounts";
+import {ActivatedRoute} from "@angular/router";
+import {URLSearchParamsUtils} from "../../shared/utils/url-search-params.utils";
+import {StatusCount} from "../../shared/model/StatusCount";
 
 @Component({
   selector: 'jm-total-summary',
@@ -9,5 +12,14 @@ import {StatusCounts} from "../../shared/model/StatusCounts";
 export class TotalSummaryComponent {
   @Input() summary: StatusCounts;
 
-  constructor() {}
+  constructor(private readonly activatedRoute: ActivatedRoute) {}
+
+  getUrlParams(statusCount: StatusCount) {
+    let map = new Map<string, string[]>();
+    map.set('projectId', [this.activatedRoute.snapshot.queryParams['projectId']]);
+    map.set('statuses', [statusCount.status.toString()]);
+
+    return {q: URLSearchParamsUtils.encodeURLSearchParamsFromMap(map)};
+  }
+
 }
