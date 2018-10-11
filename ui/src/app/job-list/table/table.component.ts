c391b7058a511ff80cccb69689ee44acd70e5cee
Rebecca Asch <rasch@broadinstitute.org>
2018-10-11 17:53:13 -0400

    fixed check for multiple bulk label values

--- ui/src/app/job-list/table/table.component.ts
+++ ui/src/app/job-list/table/table.component.ts
@@ -258,8 +258,9 @@ export class JobsTableComponent implements OnInit {
     // get default values for bulk edit fields in dialog
     for (let bulkFieldItem of this.bulkLabelFields) {
       const label = bulkFieldItem.displayField.field.replace('labels.', '');
-      bulkFieldItem.default = this.selection.selected[0].labels[label];
+      bulkFieldItem.default = this.selection.selected[0].labels[label] || '';
       for (let job of this.selection.selected) {
+        console.log(job.labels[label]);
         const jobLabelValue = job.labels[label] || '';
         if (bulkFieldItem.default !== jobLabelValue) {
           bulkFieldItem.default = null;
