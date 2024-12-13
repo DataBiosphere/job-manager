77636a9310ff5bea626459dbc7210cf4eba12d4a
Janet Gainer-Dewar <jdewar@broadinstitute.org>
2024-12-13 10:20:48 -0500

    Update test app creation

--- servers/cromwell/jobs/test/__init__.py
+++ servers/cromwell/jobs/test/__init__.py
@@ -1,6 +1,7 @@
 import logging
 
 import connexion
+from connexion.jsonifier import Jsonifier
 from flask_testing import TestCase
 
 from ..encoder import JSONEncoder
@@ -10,7 +11,11 @@ class BaseTestCase(TestCase):
 
     def create_app(self):
         logging.getLogger('connexion.operation').setLevel('ERROR')
-        app = connexion.App(__name__, specification_dir='../swagger/')
-        app.app.json_encoder = JSONEncoder
-        app.add_api('swagger.yaml')
-        return app.app
+        options = connexion.options.SwaggerUIOptions(swagger_ui=False)
+        app = connexion.App(__name__,
+                            specification_dir='../swagger/',
+                            swagger_ui_options=options,
+                            jsonifier=Jsonifier(cls=JSONEncoder))
+        app.add_api('swagger.yaml',
+                    jsonifier=Jsonifier(cls=JSONEncoder))
+        return app
