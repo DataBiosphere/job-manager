bfc6f1543fa69da826c83062d343e3f727ae389a
lmcnatt <lucymcnatt@gmail.com>
2026-03-10 11:14:25 -0400

    need tests to execute

--- ui/src/test.ts
+++ ui/src/test.ts
@@ -1,6 +1,5 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js';
 import 'zone.js/testing';
 import {getTestBed} from '@angular/core/testing';
 import {
@@ -8,16 +7,8 @@ import {
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
-// Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
-declare const __karma__: any;
-
-// Prevent Karma from running prematurely.
-__karma__.loaded = function () {};
-
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
 );
-// Finally, start Karma to run the tests.
-__karma__.start();
